ceeb9e137040f8a405dda8fd4167b3f07ea0b62b
Sergio Gomes <sergiofgomes117@gmail.com>
2017-11-10 14:33:47 -0700

    implemented navbar logic to display correct signin/signout links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sign_in user
+    if current_user 
+      (link_to "Logout", destroy_user_session_path, method: :delete, :class => "black-text")
+     else 
+      (link_to "Sign In", new_user_session_path, :class => "black-text")
+    end 
+  end
 end
